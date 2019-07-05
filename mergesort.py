f6f2f3f583b38f27e429d79d3d8f993f81bd0263
Anantvir Singh <anantvir91@gmail.com>
2019-07-05 11:19:35 -0400

    Bug fix-  wrong sentinel value

--- mergesort.py
+++ mergesort.py
@@ -7,15 +7,15 @@ def merge(A,p,q,r):
     L = list()                          # create 2 arrays to hold the elements of halved array
     R= list()
     for i in range(n_1):                    
-        L.insert(i,A[i])                # Put elements from given array 'A' into newly created L and R arrays 
+        L.insert(i,A[p+i])                # Put elements from given array 'A' into newly created L and R arrays 
         print(L[i]) 
     for j in range(n_2):
-        R.insert(j,A[q+j])
-    L.append(None)                      # Sentinel 1
-    R.append(None)                      # Sentinel 2
-    i = 1
-    j = 1
-    for x in range(len(A)):             # because we know there are total r - p + 1 elements which is = len(A)
+        R.insert(j,A[q+j+1])
+    L.append(10000000000)                      # Sentinel 1
+    R.append(10000000000)                      # Sentinel 2
+    i = 0
+    j = 0
+    for x in range(p,r+1):             # because we know there are total r - p + 1 elements which is = len(A)
         if L[i] <= R[j]:                # compare each element of L and R and put smaller one in A an move its index forward
             A[x] = L[i]
             i += 1                      # move index forward
